25e6a014657dcc4b259b8492f6851d4a6343cb3a
HyytJani <86399361+HyytJani@users.noreply.github.com>
2022-06-22 13:13:34 +0300

    neljas

--- sql.js
+++ sql.js
@@ -1,24 +1,38 @@
 
     
-    
-    var mysql=require ('mysql');
-        //import mysql from '/mysql';
-    var yhteys=mysql.createConnection({
+    const express=require("express")
+
+    const  mysql=require ('mysql');
+    const app= express()
+    const yhteys=muodostaYhteys();
+    app.use(express.static("./public"))
+
+    function muodostaYhteys(){
+    return mysql.createConnection({
         host:"localhost",
         user:"root",
         password:"",
         database:"latkaveikkaus"
 
-    }   ); 
-       
-    var server=yhteys.createServer(function(reg,err){
-        if(err) throw err;
-        console.log('toimii');
-        var sql = "delete from ottelut where joukkue1='saksa'"
-        yhteys.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Result: " + result);
-    });});
-    server.listen(3000);
+   }   ); }
+
+   app.post("/lisaa",(req,res)=>{
+    console.log(res)
+    res.end();
+   })
+   app.get("/lisaa",(res,reg)=>{
+   muodostaYhteys.connect(function(err){
+    if (err)throw err;
+    console.log('ok');
+    var sql="INSERT INTO pelaaja(etunimi,sukunimi)VALUES('petteri','hyytiainen')";
+   yhteys.query(sql,function(reg,res){
+    if(err)throw err;
+
+   });});});
+  
+
+  app.use(express.json());
+  app.listen(8000)
+  
 
     
