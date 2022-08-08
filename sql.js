db8a6cf2ba48a6e74778a34df7b8a6bcfce2ac7a
HyytJani <86399361+HyytJani@users.noreply.github.com>
2022-08-08 10:24:50 +0300

    8.8

--- sql.js
+++ sql.js
@@ -8,6 +8,8 @@
     const bodyParser=require("body-parser")
     app.use(express.static("./public"))
     app.use(bodyParser.urlencoded({extended:false}))
+    app.set('view engine','ejs');
+
 
     function muodostaYhteys(){
     return mysql.createConnection({
@@ -17,35 +19,36 @@
         database:"latkaveikkaus"
 
    }   ); }
-
-   muodostaYhteys().connect(function(err){
-    if (err)throw err;
-var sql="SELECT * FROM pelaaja";
-yhteys.query(sql,(err,results)=>{
-if(err)throw err;
-console.log(rows)});});
-
-
-
-
+   app.get('/',(req,res)=>{
+    muodostaYhteys().connect(function(err){
+        if (err)throw err;
+    sql="select * from pelaaja"
+    yhteys.query(sql,(req,rows)=>{
+        if (err)throw err;   
+            
+               res.render('index.ejs',{data:rows});
+             });
+            
+    })})
  
    app.post("/lisaa",(req,res)=>{
    
     const etuNimi=req.body.etunimi
     const sukuNimi=req.body.sukunimi
-    console.log(etuNimi,sukuNimi);
     muodostaYhteys().connect(function(err){
         if (err)throw err;
     var sql="INSERT INTO pelaaja(etunimi,sukunimi)VALUES(?,?)";
     yhteys.query(sql,[etuNimi,sukuNimi],(req,res)=>{
     if(err)throw err;
-    
-       
-   });});})
+        ;
+   });});
+   res.redirect('/')
+});
   
 
   app.use(express.json());
   app.listen(8000)
+ 
   
 
     
