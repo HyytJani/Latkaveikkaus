27b19dae7d0864aff17dfd55a88919a0f5fdd798
HyytJani <86399361+HyytJani@users.noreply.github.com>
2022-08-17 13:55:15 +0300

    17.8

--- app.js
+++ app.js
@@ -1,58 +1,79 @@
-
-    
-    const express=require("express");
+      const express=require("express");
     const http = require("http");
     const  mysql=require ('mysql');
-    const app= express()
+    const app= express();
     const yhteys=muodostaYhteys();
-    const bodyParser=require("body-parser")
-    app.use(bodyParser.urlencoded({extended:false}))
+    const bodyParser=require("body-parser");
+    app.use(bodyParser.urlencoded({extended:false}));
     app.set('view engine','ejs');
-    module.exports=require('./index.js')
 
+    //SQL YHTEYS   
     function muodostaYhteys(){
     return mysql.createConnection({
         host:"localhost",
         user:"root",
         password:"",
         database:"latkaveikkaus"
-
-   }   ); }
-   app.get('/',(req,res)=>{
-    muodostaYhteys().connect(function(err){
-        if (err)throw err;
-    sql="select * from pelaaja"
-    yhteys.query(sql,(req,rows)=>{
-        if (err)throw err;              
-               res.render('./index.ejs',{data:rows});
-             });
-            
-    })})
-    app.get('/syotapeli',(req,res)=>{
-        muodostaYhteys().connect(function(err){
-            if (err)throw err;
-        sql="select * from ottelut"
-        yhteys.query(sql,(req,rows)=>{
-            if (err)throw err;   
-                res.render('syotapeli.ejs',{data:rows});
-                 });
-                
-        })})
-        app.post("/poista",(req,res)=>{
-            var poistettava= req.body.deletebtn;
-            muodostaYhteys().connect(function(err){
-                if (err)throw err;
-            var sql="DELETE FROM pelaaja WHERE pelaajaID=(?)"        
-            yhteys.query(sql,[poistettava],(req,res)=>{
-            if(err)throw err;
-            ;});});
-            res.redirect('/');
-        });
-
-
-  app.use(express.json());
-  app.listen(8000)
- 
+   });}
   
+   muodostaYhteys().connect(function(err){
+    if (err)throw err;});
+
+   //nayta etusivu 
+   app.get('/',(req,res)=>{             
+        res.render('./index.ejs')});    
+
+    // nayta syötapeli sivu    
+    app.get('/syotapeli',(req,res)=>{       
+        sql="select * from ottelut"
+        yhteys.query(sql,(req,rows)=>{               
+        res.render('syotapeli',{data:rows});});});
+
+    //lisaa  kisa
+    app.post("/lisaakisa",(req,res)=>{   
+        const kisa=req.body.kisa        
+        var sql="INSERT INTO kisat(kisojen_nimi)VALUES(?)";
+        yhteys.query(sql,[kisa],(req,res,err)=>{
+        if(err){console.log(err)}; 
+        console.log(kisa)});              
+        res.redirect('/syotapeli')});        
+
+        
+
+    //lisaa  ottelut
+    app.post("/lisaaottelu",(req,res)=>{   
+        const peli=req.body.ottelu        
+        var sql="INSERT INTO ottelut(kisaID,ottelu)VALUES('13',?)";
+        yhteys.query(sql,[peli],(req,res,err)=>{
+        if(err)throw(err); 
+        console.log(peli)});              
+        res.redirect('/syotapeli')}); 
+
+    //nayta pelaajien hallinta sivu    
+    app.get('/hallitsepelaajia',(req,res)=>{
+        sql="select * from pelaaja"
+        yhteys.query(sql,(req,rows)=>{         
+        res.render('pelaajat.ejs',{data:rows});});});
+
+    //lisaa pelaaja
+    app.post("/lisaa",(req,res)=>{   
+        const etuNimi=req.body.etunimi
+        const sukuNimi=req.body.sukunimi
+        var sql="INSERT INTO pelaaja(etunimi,sukunimi)VALUES(?,?)";
+        yhteys.query(sql,[etuNimi,sukuNimi],(req,res,err)=>{
+        if(err)throw(err); });              
+        res.redirect('/hallitsepelaajia')});
+
+    //poista pelaaja
+    app.post('/poista',(req,res)=>{        
+        var poistettava= req.body.deletebtn;
+        var sql="DELETE FROM pelaaja WHERE pelaajaID=(?)";        
+        yhteys.query(sql,[poistettava])        
+        res.redirect('/hallitsepelaajia');})           
+
+// http yhteys        
+var server=http.createServer(app);
+server.listen(8000);
+app.use(express.json());
 
     
--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express=require("express");
+const http = require("http");
 const app= express()
 const bodyParser=require("body-parser")
 
 
 
-app.post("/lisaa",(req,res)=>{
-   
-    const etuNimi=req.body.etunimi
-    const sukuNimi=req.body.sukunimi
-    muodostaYhteys().connect(function(err){
-        if (err)throw err;
-    var sql="INSERT INTO pelaaja(etunimi,sukunimi)VALUES(?,?)";
-    yhteys.query(sql,[etuNimi,sukuNimi],(req,res)=>{
-    if(err)throw err;
-        ;});});   
-    res.redirect('/')
-});
-app.use(express.json());
+
+
+
+
